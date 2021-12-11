2f52f9d1b1aeebd542e4362a6dab2403c69b8384
Sangeeth Mathew John <65032296+sangethmathewjohn@users.noreply.github.com>
2021-12-11 21:28:30 +0530

    Update and rename code.ino to rotary_value.ino

---
+++ rotary_value.ino
@@ -0,0 +1,25 @@
+
+#include "ESPRotary.h";
+
+ESPRotary r = ESPRotary(5, 4);// D1 for CLK  
+                              // D2 for DT
+
+void Rotaryrotate(ESPRotary& r) {
+  Serial.println((r.getPosition()));
+}
+void setup()
+{
+    r.setChangedHandler(Rotaryrotate);
+
+Serial.begin(9600);
+}
+
+
+void loop()
+{
+
+    r.loop();
+
+  r.directionToString(r.getDirection());
+
+}
